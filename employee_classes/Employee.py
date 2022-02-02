5b65854714728985f2e7ff1a36ce478454e9f817
BuildTools <unconfigured@null.spigotmc.org>
2022-02-02 18:28:05 +0200

    this is question number 2 ~Adi

--- employee_classes/Employee.py
+++ employee_classes/Employee.py
@@ -8,3 +8,24 @@ def __init__(self, first_name, last_name, id_number, employee_id, manager_id, ro
     self._employee_id = employee_id
     self._manager_id = manager_id
     self._role = role
+
+
+# Getters
+def get_fist_name(self):
+    return self.__first_name
+
+def get_last_name(self):
+    return self.__last_name
+
+def get_id_number(self):
+    return self.__id_number
+
+def get_employee_id(self):
+    return self._employee_id
+
+def get_manager_id(self):
+    return self._manager_id
+
+def get_role(self):
+    return self._role
+
