3ba79c54ecc067a007ed723bd2361d66c7caad0c
BuildTools <unconfigured@null.spigotmc.org>
2022-02-02 18:19:05 +0200

    this is question number 2 ~Adi

--- employee_classes/Employee.py
+++ employee_classes/Employee.py
@@ -2,9 +2,9 @@ import Database
 from constants import *
 
 def __init__(self, first_name, last_name, id_number, employee_id, manager_id, role):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.id_number = id_number
-    self.employee_id = employee_id
-    self.manager_id = manager_id
-    self.role = role
+    self.__first_name = first_name
+    self.__last_name = last_name
+    self.__id_number = id_number
+    self._employee_id = employee_id
+    self._manager_id = manager_id
+    self._role = role
