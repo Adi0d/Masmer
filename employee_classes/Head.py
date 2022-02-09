5347c9ef10afa5fa17f372d2fdb0554be3b237c7
Shoham <sohambensimhon634@gmail.com>
2022-02-09 18:01:06 +0200

    Head

--- employee_classes/Head.py
+++ employee_classes/Head.py
@@ -1,1 +1,6 @@
 from employee_classes.Manager import *
+
+
+class Head(Manager):
+    def __init__(self, first_name, last_name, id_number, employee_id, manager_id, role):
+        super().__init__(self, first_name, last_name, id_number, employee_id, manager_id, role)
