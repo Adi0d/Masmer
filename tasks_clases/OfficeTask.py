4a635578c1f16ac165568e4ac7d82c492a565738
BuildTools <unconfigured@null.spigotmc.org>
2022-02-16 18:04:33 +0200

    טז באדר א' ה'תשפ"ב

--- tasks_clases/OfficeTask.py
+++ tasks_clases/OfficeTask.py
@@ -1,7 +1,8 @@
 from tasks_clases.Task import *
 import Database
 
-def OfficeTask(Task):
+
+class OfficeTask(Task):
     def __init__(self, task_id, status, task_type, employees_id, folder, file, involvement):
         super().__init__(task_id, status, task_type, employees_id)
         self.__folder = folder
@@ -9,7 +10,23 @@ def OfficeTask(Task):
         self.__involvement = involvement
 
         def complete_task(self, security_code):
-            pass
+            flag = False
+            if len(security_code) == 3:
+                return False
+
+            if security_code[0] == folder[0] and security_code[1] == folder[0]:
+                if security_code[2].isnumeric():
+                    if security_code[2] % 2 == 0 and involvement == "yes":
+                        flag = True
+                    elif security_code[2] % 2 == 1 and involvement == "no":
+                        flag = True
+
+            if flag:
+                Database.update_tasks_status(self._task_id)
+                self.status = "v"
+                return True
+            return False
+
 
         def get_description(self):
             return f"{self.__folder} -> {self.__file} ({self.__involvement})"
