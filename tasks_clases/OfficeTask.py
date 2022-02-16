76b46c89293982e6191de5d76b75ca8a51c5095b
BuildTools <unconfigured@null.spigotmc.org>
2022-02-16 17:52:58 +0200

    טז באדר א' ה'תשפ"ב

--- tasks_clases/OfficeTask.py
+++ tasks_clases/OfficeTask.py
@@ -1,3 +1,15 @@
 from tasks_clases.Task import *
 import Database
 
+def OfficeTask(Task):
+    def __init__(self, task_id, status, task_type, employees_id, folder, file, involvement):
+        super().__init__(task_id, status, task_type, employees_id)
+        self.__folder = folder
+        self.__file = file
+        self.__involvement = involvement
+
+        def complete_task(self, security_code):
+            pass
+
+        def get_description(self):
+            return f"{self.__folder} -> {self.__file} ({self.__involvement})"
