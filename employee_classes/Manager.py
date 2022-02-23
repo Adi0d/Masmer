5ad1dccf63cebe7fc73ffb99f0f09f200f74ae07
Shoham <sohambensimhon634@gmail.com>
2022-02-23 19:30:28 +0200

    delete employees task

--- employee_classes/Manager.py
+++ employee_classes/Manager.py
@@ -6,3 +6,15 @@ class Manager(Employee):
     def __init__(self, first_name, last_name, id_number, employee_id, manager_id, role):
         super().__init__(first_name, last_name, id_number, employee_id, manager_id, role)
         self._list_of_employees = Database.get_list_of_employees(employee_id)
+
+    def delete_employees_task(self, employees_id, task_id):
+        if task_id == "":
+            return EMPTY_FIELDS_ERROR
+        if employees_id == "":
+            employees_id = self.get_employees_id()
+            ans = Database.delete_task_from_database(employees_id, task_id)
+            if ans:
+                tasks = Database.get_list_of_tasks_from_database(employees_id)
+                for task in tasks:
+                    if task.get_task_id() == task_id:
+                        tasks.remove(task)
