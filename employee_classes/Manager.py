341ade43f53e8d279599ac857926e8a34769f28c
BuildTools <unconfigured@null.spigotmc.org>
2022-02-23 19:44:23 +0200

    כ"ג באדר א' ה'תשפ"ב

--- employee_classes/Manager.py
+++ employee_classes/Manager.py
@@ -7,6 +7,35 @@ class Manager(Employee):
         super().__init__(first_name, last_name, id_number, employee_id, manager_id, role)
         self._list_of_employees = Database.get_list_of_employees(employee_id)
 
+    def add_new_employee_task(self, employee_id, task_type, description):
+        if task_type == "" or description == "":
+            return EMPTY_FIELDS_ERROR
+        if task_type not in ["o", "f"]:
+            return TASK_TYPE_DOESNT_FIT_ERROR
+        if employee_id == "":
+            if self.get_role() == "h" and task_type == "f":
+                return TASK_TYPE_DOESNT_FIT_ERROR
+            employee_id = self.get_employees_id()
+        else:
+            add = False
+            for employee in self._list_of_employees:
+                if self.get_employees_id() == employee_id:
+                    add = True
+                    break
+            if not add:
+                return EMPLOYEES_ID_DOESNT_EXIST_ERROR
+            if task_type == "o":
+                return TASK_TYPE_DOESNT_FIT_ERROR
+        ans = Database.add_new_task_to_database(employee_id, task_type, description)
+        if ans != "":
+            if self.get_employees_id() == employee_id:
+                self._list_of_tasks.append([ans])
+            return "done"
+        else:
+            return WRONG_FORMAT_ERROR
+
+
+
     def delete_employees_task(self, employees_id, task_id):
         if task_id == "":
             return EMPTY_FIELDS_ERROR
