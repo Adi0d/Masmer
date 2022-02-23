0ea4cd94311c0b42f0aba41ffe32477195219496
Shoham <sohambensimhon634@gmail.com>
2022-02-23 19:08:33 +0200

    complete employees tasks 1

--- employee_classes/Manager.py
+++ employee_classes/Manager.py
@@ -5,3 +5,4 @@ from tasks_clases.Task import *
 class Manager(Employee):
     def __init__(self, first_name, last_name, id_number, employee_id, manager_id, role):
         super().__init__(first_name, last_name, id_number, employee_id, manager_id, role)
+        self._list_of_employees = Database.get_list_of_employees(employee_id)
